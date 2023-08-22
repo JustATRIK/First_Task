1bc5fa74a6fb6ae07d83705d2aff73e9006d4aff
JustATRIK <RArseniiP@yandex.ru>
2023-08-22 12:20:00 +0500

    Little code look change

--- src/main/java/com/example/examplemod/ClientProxy.java
+++ src/main/java/com/example/examplemod/ClientProxy.java
@@ -14,8 +14,7 @@ public class ClientProxy extends CommonProxy{
     }
 
     @Override
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         super.init(event);
     }
 
--- src/main/java/com/example/examplemod/CommonProxy.java
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -2,16 +2,27 @@ package com.example.examplemod;
 
 import com.example.examplemod.block.ModBlocks;
 import com.example.examplemod.utils.packets.EnergyAndProgressSyncPacket;
+import com.example.examplemod.utils.packets.XPRemovePacket;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
+import java.lang.reflect.Method;
+
 public class CommonProxy
 {
+    public static Method GET_DROP_METHOD;
+    public static Method GET_SILK_TOUCH_DROP;
+
     public void preInit(FMLPreInitializationEvent event) {
         ModBlocks.register();
         ExampleMod.NETWORK.registerMessage(new EnergyAndProgressSyncPacket(), EnergyAndProgressSyncPacket.class, 0, Side.CLIENT);
+        ExampleMod.NETWORK.registerMessage(new XPRemovePacket(), XPRemovePacket.class, 2, Side.SERVER);
     }
 
     public void init(FMLInitializationEvent event) {
@@ -21,5 +32,4 @@ public class CommonProxy
     public void postInit(FMLPostInitializationEvent event) {
 
     }
-
 }
