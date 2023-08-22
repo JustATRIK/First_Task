f88c7f4f5c2b403170df0fde35c81efe80c8f084
JustATRIK <RArseniiP@yandex.ru>
2023-08-22 12:21:01 +0500

    XP remove packet.

---
+++ src/main/java/com/example/examplemod/utils/packets/XPRemovePacket.java
@@ -0,0 +1,53 @@
+package com.example.examplemod.utils.packets;
+
+import com.example.examplemod.block.tiles.BlocksMinerTileEntity;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import java.util.UUID;
+
+public class XPRemovePacket implements IMessage, IMessageHandler<XPRemovePacket, IMessage> {
+    int x, y, z;
+
+    public XPRemovePacket() {
+
+    }
+
+    public XPRemovePacket(BlockPos pos) {
+        x = pos.getX();
+        y = pos.getY();
+        z = pos.getZ();
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        x = buf.readInt();
+        y = buf.readInt();
+        z = buf.readInt();
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        buf.writeInt(x);
+        buf.writeInt(y);
+        buf.writeInt(z);
+    }
+
+    @Override
+    public IMessage onMessage(XPRemovePacket message, MessageContext ctx) {
+        BlockPos blockPos = new BlockPos(message.x, message.y, message.z);
+        TileEntity tileEntity = ctx.getServerHandler().player.world.getTileEntity(blockPos);
+        if (tileEntity instanceof BlocksMinerTileEntity) {
+            System.out.println(((BlocksMinerTileEntity) tileEntity).storedXP);
+            ctx.getServerHandler().player.addExperience(((BlocksMinerTileEntity) tileEntity).storedXP);
+            ((BlocksMinerTileEntity) tileEntity).storedXP = 0;
+        }
+        return null;
+    }
+}
